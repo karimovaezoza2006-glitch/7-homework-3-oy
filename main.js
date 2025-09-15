6a10a28ed8c9cd0df420e0d2b3f8349caadd38e9
AbdujalolovaE'zoza <karimovaezoza2006@gmail.com>
2025-09-16 00:59:09 +0500

    7-homework 3-oy

---
+++ main.js
@@ -0,0 +1,89 @@
+"use strict";
+// alert ("salom dunyo")
+// let ism = ("ezoza");
+// console.log(ism);
+// let age =23;
+// console.log(age);
+// let number = 12.2;
+// console.log(number);
+// let firstName ="ezoza";
+// console.log(firstName);
+// let isUser = true;
+// console.log(isUser);
+// let student;
+// console.log(student);
+// let school = null;
+// console.log(school);
+// let age = Symbol("age");
+// console.log(typeof age);
+//  yosh =23;
+// console.log(yosh);
+// yosh=24;
+// console.log(yosh);
+// let a=5, b=10, c=15;
+// console.log(a);
+// a=b
+// console.log(a);
+// a+=b
+// console.log(a);
+// a-=b
+// console.log(a);
+// c*=b
+// console.log(c);
+// let ism = "ezoza"
+// let message = `salom ${ism}`;
+// console.log (message);
+// const yosh = 18;
+// const a = yosh>18;
+// const b = yosh<20;
+// console.log(a||b);
+// function meningFunksiyam (son1, son2){
+// return son1*son2;
+// }
+// console.log(meningFunksiyam (10, 20));
+
+// const kvadrat =function(son){
+//     return son*son;
+// }
+// console.log(kvadrat(10));
+
+
+// const salomBer = () => {
+//   console.log("Salom, dunyo!");
+// };
+// salomBer();
+// function juftSon (number){
+//     if (number%2===0){
+//         console.log(`${number} juft son`);
+//     }else{
+//         console.log(`${number}toq son`);
+//     }
+// }
+// juftSon(3);
+// function katta (number1, number2){
+//     if(number1>=number2){
+//         return ;
+//     }return number2;
+// }
+// console.log(katta(2,3));
+
+// const languages = ["js", "java", "go"]
+// console.log(languages);
+
+// alert ("salom js")
+// confirm ("siz 18damisz?")
+// prompt("Ismingizni kiriting")
+// let age = prompt("tugilgan yilingizni kiriting");
+// console.log(2025-age); 
+
+
+// let calculate = function (a, b, c){
+//     console.log(a);
+//     console.log(b);
+// }
+// calculate ();
+
+
+// let counterFunc("salom"){
+    
+// }
