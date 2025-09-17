4a9be8666968721f162c7f69baf3c584058d0375
AbdujalolovaE'zoza <karimovaezoza2006@gmail.com>
2025-09-17 06:37:31 +0500

    masala va text

--- main.js
+++ main.js
@@ -87,3 +87,110 @@
 // let counterFunc("salom"){
     
 // }
+// let result;
+// result = 4 + true;
+// console.log(result);
+
+// for(let i =1; i<10; i++){
+// console.log("Olim")
+// }
+// for(let i =10; true; i--){
+// console.log(i)}
+
+
+
+
+// 1-masala: Eng katta va eng kichik sonni topish
+function findMaxMin(sonlar) {
+  let max = sonlar[0];
+  let min = sonlar[0];
+  for (let i = 1; i < sonlar.length; i++) {
+    if (sonlar[i] > max) max = sonlar[i];
+    if (sonlar[i] < min) min = sonlar[i];
+  }
+  return { max, min };
+}
+console.log(findMaxMin([5, 12, -3, 7, 20]));
+
+
+
+// 2-masala: Tub sonni tekshirish
+function isPrime(n) {
+  if (n < 2) return false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+console.log(isPrime(17)); 
+console.log(isPrime(18)); 
+
+
+
+
+// 3-masala: Sonlarni teskari chiqarish (n dan 1 gacha)
+function reverseNumbers(n) {
+  let res = "";
+  for (let i = n; i >= 1; i--) {
+    res += i + " ";
+  }
+  return res;
+}
+console.log(reverseNumbers(10));
+
+
+
+// 4-masala: Sonning raqamlari yig‘indisi
+function digitSum(n) {
+  let sum = 0;
+  let str = n.toString();
+  for (let i = 0; i < str.length; i++) {
+    sum += Number(str[i]);
+  }
+  return sum;
+}
+console.log(digitSum(1234)); // 10
+
+
+
+// 5-masala: Sonni teskari yozish
+function reverseNumber(n) {
+  let rev = 0;
+  while (n > 0) {
+    rev = rev * 10 + (n % 10);
+    n = Math.floor(n / 10);
+  }
+  return rev;
+}
+console.log(reverseNumber(123)); // 321
+
+
+
+// 6-masala: Fibonachchi sonlari
+function fibonacci(n) {
+  let a = 0, b = 1;
+  let result = [a, b];
+  for (let i = 2; i < n; i++) {
+    let c = a + b;
+    result.push(c);
+    a = b;
+    b = c;
+  }
+  return result;
+}
+console.log(fibonacci(10));
+
+
+
+// 7-masala: Sonning bo‘luvchilari
+function divisors(n) {
+  let res = [];
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      res.push(i);
+    }
+  }
+  return res;
+}
+console.log(divisors(28)); 
+
